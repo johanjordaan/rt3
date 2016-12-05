3ed094b577208da086e712377b168d989f49d787
Johan Jordaan <johanjordaan@nightwish.local>
2016-12-05 17:21:24 +1100

    Added links in package documentation.

--- R/rt3.R
+++ R/rt3.R
@@ -4,16 +4,22 @@
 #' games. It provides a convinient gameState objescta as well as simple interface
 #' for developing new types of players.
 #'
+#'@references \url{https://en.wikipedia.org/wiki/Tic-tac-toe}
+#'
 #' @section rt3 functions:
-#' playGame
+#' \link[rt3]{playGame} Play a game of Tic-Tac-Toe
 #'
-#' startGame
-#' getMoves
-#' makeMove
-#' getWinner
+#' \link[rt3]{startGame} Start a game of Tic-Tac-Toe
 #'
-#' randomMovePlayer
-#' firstAvailableMovePlayer
+#' \link[rt3]{getMoves} Get the current set of valid moves for a given game state
+#'
+#' \link[rt3]{makeMove} Apply a move to the given game state
+#'
+#' \link[rt3]{getWinner} Get the winner in a given game state
+#'
+#' \link[rt3]{randomMovePlayer} A player that plays random valid moves
+#'
+#' \link[rt3]{firstAvailableMovePlayer} A player that alwasy plays the first move available
 #'
 #' @docType package
 #' @name rt3
